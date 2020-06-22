f1bdebb799d820ec418b39aee6be5d2d8a18a453
Keturah Howard <keturah.dev@gmail.com>
2020-06-21 19:16:54 -0700

    Refactor app to include access to mobx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {inject, observer} from 'mobx-react'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+@inject('GoalStore') //have access to this thanks to provider
+@observer // declare that this component will watch store and rerender to match changes
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        hallo
+      </div>
+    );
+  }
+  
 }
 
 export default App;
