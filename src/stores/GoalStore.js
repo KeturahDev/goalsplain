1fc49ce3fd1f3d69bac1492d1a561b48168bfcb6
Keturah Howard <keturah.dev@gmail.com>
2020-06-21 18:42:55 -0700

    create action and computed functions

--- src/stores/GoalStore.js
+++ src/stores/GoalStore.js
@@ -3,7 +3,15 @@ import {observable, action, computed} from 'mobx';
 class Goal {
   @observable goals = [];
 
-  
+  //function that performs a change to the store = action
+  @action addGoald = (goal) => {
+    this.goals.push(goal)
+  }
+
+  // any time you want to access the data and perform some sort of calculation on it, and have that calculation be reflected in real time with component, use computed
+  @computed get goalCount () {
+    return this.goals.size;
+  }
 }
 
 //garantees that information exported from this file will include the same information all throughout the various components importing this data
